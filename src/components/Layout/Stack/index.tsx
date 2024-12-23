1b67bc61e7ca6675ef93fd0eabb930dc8e276043
“sanil011” <sanilp191@gmail.com>
2024-12-23 18:27:16 +0530

    remove comment

--- src/components/Layout/Stack/index.tsx
+++ src/components/Layout/Stack/index.tsx
@@ -2,6 +2,9 @@ import { Box, BoxProps } from '../Box'
 import { cn } from '../../../utils'
 
 type StackProps = BoxProps
+
+
+
 const Stack = ({className,...props}:StackProps) => {
   return (
       <Box {...props} className={cn("flex flex-col items-start", className) } />
